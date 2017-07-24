83fbad274d60519480234ccb1aa4aa0a81cf7c42
shubham <shubham4516@gmail.com>
2017-07-24 19:32:31 +0530

    sharing video code

--- color_selection/practice.py
+++ color_selection/practice.py
@@ -1,7 +1,28 @@
-import numpy as np
-a=(np.arange(0,4))
-print(a.reshape(2,1,4))
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+import matplotlib.image as mpimg
+import os
 
+def process_image(image):
+    # NOTE: The output you return should be a color image (3 channel) for processing video below
+    # TODO: put your pipeline here,
+    # you should return the final output (image where lines are drawn on lanes)
 
-vertices = np.array([[(90,imshape[0]),(490, 290), (430, 290), (imshape[1],imshape[0])]], dtype=np.int32)
+    return image
+image = mpimg.imread("examples/laneLines_thirdPass.jpg")
+process_image(image)
+white_output = 'solidWhiteRight.mp4'
+## To speed up the testing process you may want to try your pipeline on a shorter subclip of the video
+## To do so add .subclip(start_second,end_second) to the end of the line below
+## Where start_second and end_second are integer values representing the start and end of the subclip
+## You may also uncomment the following line for a subclip of the first 5 seconds
+##clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,5)
+clip1 = VideoFileClip("solidWhiteRight.mp4")
+white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
+%time white_clip.write_videofile(white_output, audio=False)
 
+HTML("""
+<video width="960" height="540" controls>
+  <source src="{0}">
+</video>
+""".format(white_output))
